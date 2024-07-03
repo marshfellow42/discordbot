49781130f83c3fe2a2aa781c778dffac2ac27471
dark-menacing <darkmenacing@gmail.com>
2024-07-03 18:30:00 -0300

    Fixed main index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 import { bootstrapApp } from "#base";
+import { Player } from "discord-player";
 
-await bootstrapApp({ workdir: import.meta.dirname });
+await bootstrapApp({
+     workdir: import.meta.dirname,
+     beforeLoad(client) {
+        const player = new Player(client as never);
+        player.extractors.loadDefault()
+    },
+});
